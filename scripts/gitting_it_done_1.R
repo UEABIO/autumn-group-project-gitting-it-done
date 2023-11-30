70cf54e45533860fbd289ed0d3b85132e4a0dd99
kelseyconnor15 <fbc22myu@uea.ac.uk>
2023-11-30 16:44:08 +0000

    renaming some variables

--- scripts/gitting_it_done_1.R
+++ scripts/gitting_it_done_1.R
@@ -19,3 +19,16 @@ view(covid_data)
 covid_data <- janitor::clean_names(covid_data)
 
 colnames(covid_data) #quickly checks the new variable names 
+
+# renaming some of the variables 
+covid_data <- rename(covid_data,
+                     "personal_id"= "pid",
+                     "report_date"= "reprt_creationdt_false",
+                     "case_dob"="case_dob_false",
+                     "sym_start_date"="sym_startdt_false",
+                     "sym_loss_taste_smell"="sym_losstastesmell",
+                     "sym_resolved_date"="sym_resolveddt_false",
+                     "hospital_admission_date"="hosp_admidt_false",
+                     "hospital_discharge_date"="hosp_dischdt_false",
+                     "died_date"="died_dt_false",
+                     "positive_pcr_date"="pos_sampledt_false")
