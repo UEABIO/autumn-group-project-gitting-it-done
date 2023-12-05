74428554c6e62fe98ce0da3772f3a782701575f3
Jasper Raistrick <kqv22nxu@uea.ac.uk>
2023-12-05 16:07:01 +0000

    Exploring Data further and testing out different Plots. JR Script

--- scripts/JR_rscript.R
+++ scripts/JR_rscript.R
@@ -1,9 +1,24 @@
 
+# Jasper Raistrick Data Visualisation ----
 
-#Importing Data
 
-covid_data_no_duplicates %>%
-  ggplot(aes(x=case_age,
-             y = positive_pcr_)) +
-  geom_point()
 
+pallete_1 <- c("#FF8C00", "#A034F0", "#159090")
+
+covid_data_no_duplicates %>% 
+  ggplot(aes(x = hospitalized,
+             y = case_age,
+             fill = hospitalized,
+             colour = hospitalized))+
+  geom_violin(alpha = 0.2)+
+  geom_boxplot(width = 0.2,
+               alpha = 0.6)+
+  scale_fill_manual(values = pallete_1)+
+  scale_colour_manual(values = pallete_1)+
+  theme_classic()+
+  theme(legend.position = "none")+
+  labs(
+    x = "Hospitalized",
+    y = "Age",
+    title = "Age of cases who are hospitalized or not",
+    subtitle = "Box and Violin Plot")
