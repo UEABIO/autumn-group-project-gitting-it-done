9ea085fdb70651ca46d57ddd80de4b34939ec686
Jasper Raistrick <kqv22nxu@uea.ac.uk>
2023-12-05 19:58:01 +0000

    Filtering out Yes symptoms and trying to combine- JR

--- scripts/JR_rscript.R
+++ scripts/JR_rscript.R
@@ -9,54 +9,38 @@
 
 # Selecting just symptom variables from dataset using dplyr and naming as object
 
-covid_symptoms_data <- select(.data = covid_data_no_duplicates, 
-       sym_fever, sym_subjfever, sym_myalgia, sym_loss_taste_smell,
+covid_symptoms_data <-  covid_data_no_duplicates %>% 
+       select(sym_fever, sym_subjfever, sym_myalgia, sym_loss_taste_smell,
        sym_sorethroat, sym_cough, sym_headache)
 
 
 # Filtering out 'No', 'Unknown' and 'N/a' results for all symptoms
 # And creating new object of filtered dataset 'symptpms_yes_data'
-symptoms_yes_data <- filter(.data = covid_symptoms_data, 
-                            sym_fever == "Yes", sym_subjfever == "Yes", 
+symptoms_yes_data <- covid_symptoms_data %>%  
+                            filter(sym_fever == "Yes", sym_subjfever == "Yes", 
                             sym_myalgia == "Yes", sym_loss_taste_smell == "Yes",
                             sym_sorethroat == "Yes", sym_cough == "Yes",
                             sym_headache == "Yes") 
 
-
-symptoms_yes_data %>% 
-  glimpse()
-
-symptoms_yes_data %>% 
-  count(sym_loss_taste_smell)
-
-
-symptoms_yes_data %>%
-  ggplot(aes(x=)) +
-  geom_bar()
-  
-sym_fever_yes <- covid_symptoms_data %>% 
+sym_fever_yes <- covid_symptoms_data %>%
   filter(sym_fever == "Yes") %>% 
-  select(sym_fever) %>% 
+  select(sym_fever)
+
+
+
+ glimpse(sym_fever_yes)
+
+
+ sym_cough_yes <- covid_symptoms_data %>%
+   filter(sym_cough == "Yes") %>% 
+   select(sym_cough)
+
+ glimpse(sym_cough_yes)
+
+merge(sym_cough_yes, sym_fever_yes) %>% 
   glimpse()
-
-
-sym_fever_yes %>% 
-  ggplot(aes(x = sym_fever)) +
-  geom_bar()
-
-
-
-
  
-  sym_fever_yes_count <- covid_symptoms_data %>% 
-    filter(sym_fever == "Yes") %>%
-    count(sym_fever)
-
-  
-  covid_symptoms_data %>% 
-    ggplot(aes(y = sym_fever)) +
-    geom_bar()
-  
+ 
   
   
   
