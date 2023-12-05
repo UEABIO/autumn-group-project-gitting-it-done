26b812ab85157c346a9f51dbbd0f4b90d3b3cff2
Jasper Raistrick <kqv22nxu@uea.ac.uk>
2023-12-05 18:11:28 +0000

    Exploring Symptoms shown and filtering data

--- scripts/JR_rscript.R
+++ scripts/JR_rscript.R
@@ -2,23 +2,57 @@
 # Jasper Raistrick Data Visualisation ----
 
 
+# What are the most common covid symptoms recorded? ----
+#_________________----
 
-pallete_1 <- c("#FF8C00", "#A034F0", "#159090")
+# Filtering and Selecting variables ----
 
-covid_data_no_duplicates %>% 
-  ggplot(aes(x = hospitalized,
-             y = case_age,
-             fill = hospitalized,
-             colour = hospitalized))+
-  geom_violin(alpha = 0.2)+
-  geom_boxplot(width = 0.2,
-               alpha = 0.6)+
-  scale_fill_manual(values = pallete_1)+
-  scale_colour_manual(values = pallete_1)+
-  theme_classic()+
-  theme(legend.position = "none")+
-  labs(
-    x = "Hospitalized",
-    y = "Age",
-    title = "Age of cases who are hospitalized or not",
-    subtitle = "Box and Violin Plot")
+# Selecting just symptom variables from dataset using dplyr and naming as object
+
+covid_symptoms_data <- select(.data = covid_data_no_duplicates, 
+       sym_fever, sym_subjfever, sym_myalgia, sym_loss_taste_smell,
+       sym_sorethroat, sym_cough, sym_headache)
+
+
+# Filtering out 'No', 'Unknown' and 'N/a' results for all symptoms
+# And creating new object of filtered dataset 'symptpms_yes_data'
+symptoms_yes_data <- filter(.data = covid_symptoms_data, 
+                            sym_fever == "Yes", sym_subjfever == "Yes", 
+                            sym_myalgia == "Yes", sym_loss_taste_smell == "Yes",
+                            sym_sorethroat == "Yes", sym_cough == "Yes",
+                            sym_headache == "Yes") 
+
+
+symptoms_yes_data %>% 
+  glimpse()
+
+symptoms_yes_data %>% 
+  count(sym_loss_taste_smell)
+
+
+symptoms_yes_data %>%
+  ggplot(aes(x=)) +
+  geom_bar()
+  
+
+  
+  
+  covid_symptoms_data %>% 
+  count(sym_fever)
+ 
+  covid_symptoms_data %>% 
+    filter(sym_fever== "Yes") %>%
+    count(sym_fever)
+  
+  
+  
+  
+  
+  
+  
+#_________________----
+# % of people with symptoms ----
+# Working out Covid symptoms shown as a proportion of total positive PCR tests
+
+
+
