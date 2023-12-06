917bac9b1e4004c3fa13d89a728794089249a531
kelseyconnor15 <fbc22myu@uea.ac.uk>
2023-12-06 16:34:33 +0000

    adding to the KC script, changing my idea for the figure

--- scripts/KC_rscript.R
+++ scripts/KC_rscript.R
@@ -1,7 +1,8 @@
-# Differences in symptom start date and resolution date between members of the Hispanic and non-Hispanic communities
+# Differrences in rates of death between hispanic and other races 
+## Hispanic/latinos are more likely to die from covid compared to white 
 
 # Setting up script ----
-rm(list=ls())
+
 
 # loading packages 
 library(tidyverse)
@@ -15,6 +16,7 @@ library(ggthemes)
 # Using the select function to only include certain variables ----
 
 # creating the data set ----
+
 head(covid_data_no_duplicates)
 
 covid_data_ethnicty_vs_symptons<- select(.data = covid_data_no_duplicates, 
@@ -22,5 +24,8 @@ covid_data_ethnicty_vs_symptons<- select(.data = covid_data_no_duplicates,
   view()
 
 
-
+covid_data_no_duplicates %>% 
+  ggplot(aes(x=case_eth, 
+             y = sym_start_date))+
+  geom_point() 
 
