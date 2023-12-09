e878b3d474a07f51a7e49061e389b7da247dcf38
JessCunliffe9 <nnq21gdu@uea.ac.uk>
2023-12-09 13:58:23 +0000

    finalising data manipulaton and creating graph

--- scripts/JC_rscript.R
+++ scripts/JC_rscript.R
@@ -1,5 +1,9 @@
 # Correlation between age, gender and hospitalization rate 
 
+# I want to look at the relationship between sex and rate of hospitalization 
+# I want to look at the relationship between age and rate of hospitalization
+# I want to create a visual which encompasses both factors and hospitalization 
+
 rm(list=ls())
 
 #Setting up Script ----
@@ -28,7 +32,6 @@ covid_data_age_gender_hospitalized <- arrange(.data = covid_data_age_gender_hosp
 # remove any unrelaistic ages(-20)
 # total males hospitalised 
 # total females hospitalised
-# average age
 
 # Managing the NA values ---- 
 
@@ -59,11 +62,13 @@ covid_data_age_gender_hospitalized_2 %>%
 
 # 81 Genders unknown, however cannot discard this data as data entry may not have accounted for non-binary/trans community
 
+# Data Manipulation ----
+
 # Totals of each gender of each age 
 totals_age_gender <- covid_data_age_gender_hospitalized_2 %>% 
   group_by(case_age, case_gender) %>% 
-  summarise(num=n())
-
+  summarise(num=n()) 
+  
   view(totals_age_gender)
 
 # totals of each gender and age separated by hospitalized or not
@@ -82,28 +87,28 @@ age_vs_hospitalisation_data <- covid_data_age_gender_hospitalized_2 %>%
   filter(hospitalized == "Yes", !case_gender == "Unknown") %>% 
   summarise(n = n())
   
-  
-  mutate(prob = n/sum()) 
-
-# INCORRECT need to be out of the number of total number of gender of that age
-
 view(age_vs_hospitalisation_data)
 
+# creating a data set with percentage hospitalised from age and gender 
 
-age_vs_hospitalisation_data %>% 
+age_gender_total_hosp <- merge(age_vs_hospitalisation_data, totals_age_gender, by=c("case_age","case_gender")) %>% 
+  mutate(percentage = (n/num)*100)
+
+view(age_gender_total_hosp)
+
+# Creating Graph ----
+
+age_gender_total_hosp %>% 
   ggplot(aes(x = case_age, 
-             y = prob,
+             y = percentage,
              colour = case_gender))+
-  geom_point()
-  theme_minimal()
+  geom_smooth()+
+  labs(x = "Patient Age",
+       y = "% of Patients Hospitalised")
 
-# NEED TO CREATE TWO SEPERATE DATA SETS TO LOOK AT RELATIONSHIP BETWEEN GENDER AND HOSPITALISATION 
+  # Options to add
+  geom_smooth(method="lm") # line of best fit 
 
-# I want to look at the relationship between sex and rate of hospitalization 
-# I want to look at the relationship between age and rate of hospitalization
-# I want to create a visual which encompasses both factors and hospitalization 
-  
-
-  
+  coord_flip() # flips axis 
   
   
