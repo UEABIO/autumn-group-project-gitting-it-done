148d914ae8dacc7b804943a20f636fe3b7c46cda
willclifton62 <nmt22psu@uea.ac.uk>
2023-12-11 19:03:54 +0000

    Updated script

--- scripts/WC_rscript.R
+++ scripts/WC_rscript.R
@@ -41,5 +41,14 @@ covid_data_age_gender_time_hospital <- covid_data_age_gender_time_hospital %>%
   filter(!case_gender == "Unknown") %>% 
   group_by(case_age, case_gender, hospital_admission_date, hospital_discharge_date)
 
+covid_data_hospital_time <- covid_data_age_gender_time_hospital %>%
+  summarise(admission = min(hospital_admission_date),
+            discharge = max(hospital_discharge_date),
+            hospital_duration = discharge - admission,
+            n = n())
+
+covid_data_age_gender_time_hospital <- covid_data_age_gender_time_hospital %>%
+  mutate(hospital_duration = as.numeric(difftime(hospital_discharge_date, hospital_admission_date, units = "days")))
+
 view(covid_data_age_gender_time_hospital)
 
