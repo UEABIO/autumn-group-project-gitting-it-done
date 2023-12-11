9831122b9d0f33b7a706d572f5ff757f21e199e9
kelseyconnor15 <fbc22myu@uea.ac.uk>
2023-12-11 12:31:14 +0000

    Starting KC graph

--- scripts/KC_rscript.R
+++ scripts/KC_rscript.R
@@ -1,5 +1,4 @@
-# Differrences in rates of death between hispanic and other races 
-## Hispanic/latinos are more likely to die from covid compared to white 
+# what percentage of people died died of covid 
 
 # Setting up script ----
 
@@ -13,19 +12,42 @@ library(plotly)
 library(here)
 library(ggthemes)
 
+#creating objects
+
 # Using the select function to only include certain variables ----
 
 # creating the data set ----
 
 head(covid_data_no_duplicates)
 
-covid_data_ethnicty_vs_symptons<- select(.data = covid_data_no_duplicates, 
-       sym_start_date , sym_resolved_date , case_eth) %>% 
+covid_died_vs_died_covid_by_race <- select(.data = covid_data_no_duplicates, 
+       died , died_covid, case_race) %>% 
   view()
 
+# getting rid of the n/a values 
+covid_died_vs_died_covid_by_race <- drop_na(covid_died_vs_died_covid_by_race)
 
+covid_died_vs_died_covid_by_race %>% 
+  is.na() %>% 
+  sum()
+
+glimpse(covid_died_vs_died_covid_by_race)
+
+# summarising the data
+covid_died_vs_died_covid_by_race %>% 
+  summarise(n=n(),
+            quanitity_died = sum(died_covid == "yes"),
+
+
+# starting to create the graph  
 covid_data_no_duplicates %>% 
-  ggplot(aes(x=case_eth, 
-             y = sym_start_date))+
-  geom_point() 
-
+  ggplot(aes(x=died, 
+             y = died_covid,
+             colour= ))+ 
+  geom_point()+
+  geom_smooth(method="lm",    
+              se=FALSE)+
+  labs(x = "Died",
+       y = "Died of covid",
+       title= "Died vs died of covid amoungst different races",
+       subtitle= "")
