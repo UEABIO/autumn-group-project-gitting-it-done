967707bbddc11212029613dba259f1ea051fd141
Jasper Raistrick <kqv22nxu@uea.ac.uk>
2023-12-12 13:38:39 +0000

    Mutated symptom columns, changed values to NA, new symptoms column created, removed NA's -JR

--- scripts/JR_rscript.R
+++ scripts/JR_rscript.R
@@ -16,11 +16,7 @@ library(here)
 library(ggthemes)
 
 
-
-
 # Filtering and Selecting variables ----
-
-
 
 # Selecting just symptom variables from dataset using dplyr and naming as object
 
@@ -29,36 +25,57 @@ covid_symptoms_data <-  covid_data_no_duplicates %>%
        sym_sorethroat, sym_cough, sym_headache)
 
 
+# Getting a sum of how many observations are missing in dataframe (N/a)
+covid_symptoms_data %>% 
+  is.na() %>% 
+  sum()       # 248173 observations are missing
 
+summary(covid_symptoms_data)
 
-# Filtering out 'No', 'Unknown' and 'N/a' results for all symptoms
-# And creating new object of filtered dataset 'symptpms_yes_data'
-symptoms_data_group <- covid_symptoms_data %>%  
-                            group_by(sym_fever, sym_subjfever, 
-                            sym_myalgia, sym_loss_taste_smell ,
-                            sym_sorethroat, sym_cough,
-                            sym_headache) %>% 
-  summarise(num=n(na.rm = TRUE))
-
-
-glimpse(symptoms_data_group)
+# Changing value names in each column from "Yes" to their symptom name using mutate
 
 
 
-  
-symptoms_yes_data <- covid_symptoms_data %>%  
-  filter(sym_fever == "Yes", sym_subjfever == "Yes", 
-         sym_myalgia == "Yes", sym_loss_taste_smell == "Yes",
-         sym_sorethroat == "Yes", sym_cough == "Yes",
-         sym_headache == "Yes") 
-  
-  
-  
-  
-  
-  
-  
-# Advised: Use group by and count  functions and summarise
+covid_symptoms_data_clean <- covid_symptoms_data %>% 
+  mutate(sym_cough = case_when(sym_cough == "Yes" ~ "cough",
+                             sym_cough == "Unk" ~ "NA",
+                             sym_cough == "No" ~ "NA"),
+         sym_fever = case_when(sym_fever == "Yes" ~ "fever",
+                               sym_fever == "Unk" ~ "NA",
+                               sym_fever == "No" ~ "NA"),
+         sym_headache = case_when(sym_headache == "Yes" ~ "headache",
+                               sym_headache == "Unk" ~ "NA",
+                               sym_headache == "No" ~ "NA"),
+         sym_subjfever = case_when(sym_subjfever == "Yes" ~ "subjfever",
+                               sym_subjfever == "Unk" ~ "NA",
+                               sym_subjfever == "No" ~ "NA"),
+         sym_myalgia = case_when(sym_myalgia == "Yes" ~ "myalgia",
+                               sym_myalgia == "Unk" ~ "NA",
+                               sym_myalgia == "No" ~ "NA"),
+         sym_sorethroat = case_when(sym_sorethroat == "Yes" ~ "sorethroat",
+                               sym_sorethroat == "Unk" ~ "NA",
+                               sym_sorethroat == "No" ~ "NA"),
+         sym_loss_taste_smell = case_when(sym_loss_taste_smell == "Yes" ~ "loss_taste_smell",
+                               sym_loss_taste_smell == "Unk" ~ "NA",
+                               sym_loss_taste_smell == "No" ~ "NA"))
+
+
+
+# Now to combine these columns into a new colum called symptoms ----
+
+# Changing dataframe to long format
+long_symptoms_data_clean <- covid_symptoms_data_clean %>% 
+  pivot_longer(cols = sym_fever:sym_headache,
+                                         names_to = "Type",
+                                         names_prefix = "sym",
+                                         values_to = "Symptoms")
+
+# Removing NA values from new Symptoms column
+
+long_symptoms_data_clean %>% 
+  drop_na(Symptoms) %>% 
+  filter(Symptoms != "NA") 
+ 
 
 
 
@@ -69,13 +86,15 @@ symptoms_yes_data <- covid_symptoms_data %>%
 
 
 
-
-
-  
-
-  #_________________----
+#_________________----
 # % of people with symptoms ----
 # Working out Covid symptoms shown as a proportion of total positive PCR tests
+
+
+
+
+
+
 
 
 
