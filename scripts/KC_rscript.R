44579aedb1ed9eae306725c3f268c4e5d96b81fa
kelseyconnor15 <fbc22myu@uea.ac.uk>
2023-12-14 16:53:58 +0000

    KC- adding a title to my coding

--- scripts/KC_rscript.R
+++ scripts/KC_rscript.R
@@ -1,4 +1,8 @@
-# Mortality Patterns by Race: General Death vs Deaths Attributed by COVID-19 ----
+# KC Covid Outbreak Situation Report ----
+# A boxplot to understand the distribution between Age and Race using data from a Covid Report----
+
+# clearing the environment
+# rm(list=ls())
 
 # loading the packages ----
 library(tidyverse)
@@ -9,7 +13,7 @@ library(plotly)
 library(here)
 library(ggthemes)
 
-# Creating a box plot to show case_age and case_race 
+# Creating a box plot to show case_age and case_race ----
 covid_data_race_and_sym_numeric <- covid_data_no_duplicates
 
 # Remove NAs from case_age variable
@@ -38,7 +42,7 @@ covid_data_race_and_sym_numeric <- covid_data_race_and_sym_numeric %>%
 unique(covid_data_race_and_sym_numeric$case_race)
 unique(covid_data_race_and_sym_numeric$case_age)
 
-# Creating a box plot exploring case and age 
+# Creating a box plot exploring case and age ----
 age_and_race_boxplot_KC <- ggplot(covid_data_race_and_sym_numeric, aes(x = case_race, y = case_age, fill = case_race)) +
   geom_boxplot(outlier.shape = 16, alpha = 0.7, position = position_dodge(width = 0.8)) +
   stat_ellipse(aes(color = case_race), type = "norm", level = 0.95, position = position_dodge(width = 0.8)) +
