a90ff7246f75fe5b1964e80fb930d65fdac2f269
Jasper Raistrick <kqv22nxu@uea.ac.uk>
2023-12-15 19:37:46 +0000

    JR script- Changing percentage label decimal places

--- scripts/JR_rscript.R
+++ scripts/JR_rscript.R
@@ -96,12 +96,20 @@ group_by( type, symptoms) %>%  # Creating new column of totals
 
 # Creating a bar chart
 
-
-
+# Creating proportions of Yes and No symptoms data
 proportion_covid_data <- covid_symptoms_data_clean_long %>% 
   group_by(type) %>% 
-  mutate(prop_sym = n/sum(n)) %>%  # Creating proportions of Yes and No symptoms data
-    filter(symptoms != "No") %>%  # Filtering out No proportions
+  mutate(prop_sym = n/sum(n)) %>%  
+    filter(symptoms != "No") # Filtering out No proportions
+
+
+
+# DATA VISUALISATION ----
+
+
+# Creating a bar chart
+
+covid_symptoms_bar_chart <- proportion_covid_data %>% 
     ggplot(aes(y = type, x = prop_sym, fill = type)) +
   geom_col() +
   scale_fill_manual(values = c( "sym_cough" = "darkblue",
@@ -111,10 +119,18 @@ proportion_covid_data <- covid_symptoms_data_clean_long %>%
                                 "sym_loss_taste_smell"= "royalblue",
                                 "sym_sorethroat" = "lightgrey",
                                 "sym_myalgia" = "lightgrey")) +
-  geom_text(aes(label=scales::percent(prop_sym)), nudge_x = -0.06) +
-  theme_minimal()
+geom_label(aes(label=scales::percent(prop_sym, accuracy = 0.1)), 
+            nudge_x = -0.05,
+            fill = "white",
+            fontface = "bold",
+            family = "Fira Sans") +
+  theme_minimal() +
+  theme(legend.position = "none") # Removing legend
+
+theme(legend.position = "none",
+      axis.text.y = element_text(size = 14, hjust = 1, family = "Fira Sans"))
     
-proportion_covid_data
+covid_symptoms_bar_chart
 
 
 
