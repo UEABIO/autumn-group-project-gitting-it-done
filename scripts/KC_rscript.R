1821621797d6daf9a093d9a324971df2e569502d
kelseyconnor15 <fbc22myu@uea.ac.uk>
2023-12-20 16:45:13 +0000

    KC- we encountered a merge conflict so my changes were deleted. Luckily I had pushed them to GIT HUB previously so this is me copying them back into my code and adding # comments to all of my lines of code.

--- scripts/KC_rscript.R
+++ scripts/KC_rscript.R
@@ -13,60 +13,74 @@ library(plotly)
 library(here)
 library(ggthemes)
 
-# Creating a box plot to show case_age and case_race ----
+# Renaming the data set before creating the box plot  ----
 covid_data_race_and_sym_numeric <- covid_data_no_duplicates
 
-# Remove NAs from case_age variable
+# Remove NAs from case_age variable ----
 covid_data_race_and_sym_numeric <- covid_data_race_and_sym_numeric[complete.cases(covid_data_race_and_sym_numeric$case_age), ]
 
-# Remove NAs from case_race variable
+# Remove NAs from case_race variable ----
 covid_data_race_and_sym_numeric <- covid_data_race_and_sym_numeric[complete.cases(covid_data_race_and_sym_numeric$case_race), ]
 
 # Check the resulting dataset
 head(covid_data_race_and_sym_numeric)
 
-# Remove rows with "other", "unknown" and "under review" values in case_race
+# Remove rows with "other", "unknown" and "under review" values in case_race ----
 covid_data_race_and_sym_numeric <- covid_data_race_and_sym_numeric %>%
   filter(!(case_race %in% c("UNKNOWN", "OTHER", "UNDER REVIEW", "NA", NA)))
 
-# Remove rows with "other", "unknown" and "under review" values in symptom_count
+# Remove rows with "other", "unknown" and "under review" values in symptom_count ----
 covid_data_race_and_sym_numeric <- covid_data_race_and_sym_numeric %>%
   filter(!(case_age %in% c("UNKNOWN", "OTHER", "UNDER REVIEW", "NA")))
 
-# getting rid of the -20 age from case_age
+# getting rid of the -20 age from case_age ----
 covid_data_race_and_sym_numeric <- covid_data_race_and_sym_numeric %>%
   filter(case_age >= 0,
          case_age<= 110)
 
-# USING THE UNIQUE FUNCTION TO CHECK THIS WORKED
+# Using the unique function to check this worked -----
 unique(covid_data_race_and_sym_numeric$case_race)
 unique(covid_data_race_and_sym_numeric$case_age)
 
-# Creating a box plot exploring case and age ----
-age_and_race_boxplot_KC <- ggplot(covid_data_race_and_sym_numeric, aes(x = case_race, y = case_age, fill = case_race)) +
-  geom_boxplot(outlier.shape = 16, alpha = 0.7, position = position_dodge(width = 0.8)) +
-  stat_ellipse(aes(color = case_race), type = "norm", level = 0.95, position = position_dodge(width = 0.8)) +
-  scale_fill_manual(values = c("mediumpurple", "paleturquoise", "seagreen2", "lightgoldenrod1", "lightpink")) +
-  labs(title = "Figure 3. Box plot to show age and race",
-       subtitle = "Graphically demonstarting the distribution of age and race",
-       caption = "Figure 3. A boxplot to demonstrate the relationshiop between age and race in the Covid Outbreak data set. The box plot shows the minimum, maximum, medium age as well as the upper and lower quartile.",
-       x = "Race",
-       y = "Age") +
-  theme_minimal() +
-  theme(axis.title.x = element_text(size = 12),
-        axis.title.y = element_text(size = 12),
-        axis.text.x = element_text(size = 10, angle = 45, hjust = 1), # Rotate x-axis labels by 45 degrees
-        axis.text.y = element_text(size = 10),
-        plot.title = element_text(size = 14, face = "bold"),
-        legend.position = "top",
-        legend.title = element_text(size = 12), 
-        legend.text = element_text(size = 10),
-        panel.grid.major = element_line(color = "gray", size = 0.2),
-        panel.grid.minor = element_blank(),
-        legend.background = element_rect(fill = "white", color = "black", size = 0.5),
-        panel.border = element_rect(color = "black", fill = NA, size = 0.7),
-        strip.background = element_rect(fill = "white", color = "black", size = 0.7),
-        strip.text = element_text(face = "bold")) +
-  guides(fill = guide_legend(override.aes = list(alpha = 1))) +
-  scale_color_manual(values = c("mediumpurple", "paleturquoise", "seagreen2", "lightgoldenrod1", "lightpink")) +
-  labs(fill = "Race:")
+
+# creating an age race box plot ----
+age_and_race_boxplot_KC <- ggplot(covid_data_race_and_sym_numeric, aes(x = case_race, y = case_age, fill = case_race)) + #plotting the x and y axis 
+  geom_boxplot(outlier.shape = 16, alpha = 0.7, position = position_dodge(width = 0.8)) + # adds the data in a  boxplot 
+  stat_ellipse(aes(color = case_race), type = "norm", level = 0.95, position = position_dodge(width = 0.8)) + # adds ellipses to the plot, representing the normal distribution of the case_race data set 
+  scale_fill_manual(values = c("mediumpurple", "paleturquoise", "seagreen2", "lightgoldenrod1", "lightpink")) + #manually setting the colours for the different races 
+  labs(title = "Unpacking COVID-19 Demographics: An insight into the link between Age and Race.", #adding a title to the explain the graph 
+       subtitle = "Examining Variability and Trends in Age Distribution among Different Racial Groups; as well as showing the median ages.", #adding a subtitle to add further information about the boxplot 
+       x = "Race", # remaning the x axis 
+       y = "Age") + # remaning the y axis 
+  scale_x_discrete(labels = NULL) +  # Remove x-axis labels as they are already in the legend 
+  theme_minimal() + #same theme as the rest of the group to make the plots look good together
+  theme(
+    axis.title.y = element_text(size = 12), # to adjust the size of the title on the x-axis
+    axis.text.x = element_blank(),  # Remove x-axis labels as they are already in the figure legend
+    axis.text.y = element_text(size = 10), # to adjust the size of the title on the y-axis
+    plot.title = element_text(size = 14, face = "bold"), #making the title bold and big 
+    legend.position = "top", #position of the title on the page 
+    legend.title = element_text(size = 12), #adjusting the size of the title in the legend box
+    legend.text = element_text(size = 10), #adjusting the size of the text in the legend box
+    panel.grid.major = element_line(color = "gray", size = 0.2), #to change the colour and size of the major grid lines
+    panel.grid.minor = element_blank(), #can adjust the minor grid lines if needed
+    legend.background = element_rect(fill = "white", color = "black", size = 0.5), #background colour of the legend can be adjusted 
+    panel.border = element_rect(color = "black", fill = NA, size = 0.7), #allows us to change the colour and size of the panel border
+    strip.background = element_rect(fill = "white", color = "black", size = 0.7), #allows us to change the colour and size of the strip background
+    strip.text = element_text(face = "bold") # making the text bold 
+  ) +
+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + 
+  scale_color_manual(values = c("mediumpurple", "paleturquoise", "seagreen2", "lightgoldenrod1", "lightpink")) + #manually setting the colours for the different races 
+  labs(fill = "Race:") + 
+  
+  
+# Adding labels to the boxplot to show the median age values ----
+stat_summary( # used to calculate summary statistics and display them on the box plot 
+  fun = "median", # the fuction i want to be applied is median 
+  geom = "text", #displays it in a text format 
+  position = position_dodge(width = 0.8), #controls the position of the values 
+  aes(label = sprintf("Median: %.1f", ..y..)), #specifies how the data should be mapped
+  size = 3, #changing the size of the values 
+  vjust = -1  # Adjust the vertical position of the labels
+) 
+print(age_and_race_boxplot_KC) # allows us to see the plot 
